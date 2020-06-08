9aa55081aa311da8c6ed2773e6f514f6bdb637d2
mdomeck <mdomeck@aol.com>
2020-06-08 19:26:54 -0400

    added content, started sales-html

--- js/app.js
+++ js/app.js
@@ -5,8 +5,26 @@
 // Display the values of each array as unordered lists in the browser
 // Calculating the sum of these hourly totals; your output for each location should look like this:
 
+var seattleStore = {
+  name : 'Seattle',
+  min : 23 ,
+  max : 65 ,
+  avg : 6.3 ,
+  renderToPage : function(){
+    var parentUnorderedList = document.getElementById('seattle-ul');
+    var listItem = document.createElement('li');
+    listItem.textContent = this.name;
+    parentUnorderedList.appendChild(listItem);
+ 
+    var hoursOfOperation = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm', hourlyTotals ];
 
+    for(var i = 0; i < hoursOfOperation.length; i++){
+      (randomNumberCustomer*6.3);
+      listItem = document.createElement('li');
+      listItem.textContent = hoursOfOperation[i] + ':' + avg + 'cookies';
+      parentUnorderedList.appendChild(listItem);
 
+  }
+}
 
-
-
+seattleStore.renderToPage()
